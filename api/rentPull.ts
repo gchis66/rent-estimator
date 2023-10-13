6deaabf31d4a3b84f60d6900bb01220a67092fd4
gchis66 <gchis66@gmail.com>
2023-10-12 22:13:32 -0400

    dotenv shit removed

--- api/rentPull.ts
+++ api/rentPull.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 // Load environment variables from .env file
 config();
 const url = "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice";
-const apiKey = process.env.RENT_API_KEY;
+const apiKey = "3b3912e780msh41ff7b33b453ba0p1477edjsn3a2d1357e6a2";
 
 // Export an async function to make the API call
 export async function rentPull(
